1ebd15ff2a2960ae2874eef6887005cf74445e84
Carsten <engelhardtcarsten00@gmail.com>
2025-12-02 20:01:29 +0100

    create components

---
+++ src/app/component/myself/myself.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-myself',
+  imports: [],
+  templateUrl: './myself.component.html',
+  styleUrl: './myself.component.scss'
+})
+export class MyselfComponent {
+
+}
